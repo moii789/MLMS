7a0869f39b36b1ad6644e56f37cd06a4100ec97e
Prince John <prince.apple.john@gmail.com>
2020-10-09 15:01:15 -0400

    Added the automated QR code emailing functionality. Set SMTP password as an enviroment variable before running.

--- databasecapstone/makerlab/views.py
+++ databasecapstone/makerlab/views.py
@@ -4,7 +4,9 @@ from django.contrib.auth import authenticate
 from django.http import JsonResponse
 from . models import RegisteredUser, EntryExit, Item, InUseMachine
 from . import functions as functions
-
+import qrcode
+from django.core.mail import EmailMessage
+from email.mime.image import MIMEImage
 import json
 import datetime
 
@@ -39,6 +41,7 @@ def register(request):
 
     new_user = RegisteredUser.objects.create(user_id = user_id, first_name = first_name, last_name = last_name, date_of_birth = date_of_birth, email = email, visitor_type = visitor_type, student_id = student_id )
     new_user.save()
+    send_qr_email(user_id, email)
     return JsonResponse({ 'success': True, 'data': 'Nothing'})
 
 
@@ -94,4 +97,18 @@ def handle_items(request):
             new_in_use_machine.save()
         return JsonResponse({'success': True, 'data': 'Nothing', 'message': 'machine records saved'})
 
+
+def send_qr_email(unique_identifier, email_address):
+    """ Enter the chosen unique id to be encoded as unique_identifier and query the members table to get the
+    email address of that record """
+    code = qrcode.make(unique_identifier)
+    code.save('makerlab/temp/temp_code.png', 'PNG')
+    msg: EmailMessage = EmailMessage(subject='Welcome to MakerLab at CELEB',
+                                     body='This is an automated email. The QR code can be used check in/out rapidly '
+                                          'at the Makerlab. Your unique ''identifier is: ' + str(unique_identifier),
+
+                                     from_email='makerlab@beloit.edu', to=[email_address])
+    code = open(r'makerlab/temp/temp_code.png', 'rb')
+    msg.attach(MIMEImage(code.read()))
+    msg.send(fail_silently=False)
 
